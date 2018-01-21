3eb681d98e2212f86fd336cc1ce1210b6dadfce0
Karen O <k.ortiz026@gmail.com>
2018-01-21 03:57:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  //randomly-generate price btwn 0-100
+  var itmPrice = Math.floor(Math.random() *100); 
+  
+  //Use key-value pair to create obj for item
+  //Add to cart array using push
+  cart.push({[item]:itmPrice});
+  
+  //Print item has been added to cart.
+  console.log(`${item} has been added to your cart.`);
+  
+  //Return cart
+  return cart;
 }
 
 function viewCart() {
